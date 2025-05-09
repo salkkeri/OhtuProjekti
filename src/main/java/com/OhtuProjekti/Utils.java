73fb4db412e632e1deb20f7e7978341f506684c0
ananpn <anttpw@gmail.com>
2025-05-09 13:47:48 +0300

    Added utils

---
+++ src/main/java/com/OhtuProjekti/Utils.java
@@ -0,0 +1,23 @@
+package com.OhtuProjekti;
+
+/**
+ * Various constants and functions used globally
+ * @author Antti
+ */
+public class Utils {
+
+    /**
+     * Color of the background
+     */
+    public static final String BACKGROUNDCOLOR = "-fx-background-color: #030303";
+
+    /**
+     * Window width
+     */
+    public static final double SCREEN_WIDTH = 1200;
+    /**
+     * Window height
+     */
+    public static final double SCREEN_HEIGHT = 800;
+
+}
