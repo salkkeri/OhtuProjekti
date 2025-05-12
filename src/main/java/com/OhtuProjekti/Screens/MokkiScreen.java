977cf79baa2666edbd84eb9db4f0049d78ea45c8
Justus Talvitie <justus.talvitie@protonmail.com>
2025-05-12 15:41:42 +0300

    Toimiva mökkiscreen

--- src/main/java/com/OhtuProjekti/Screens/MokkiScreen.java
+++ src/main/java/com/OhtuProjekti/Screens/MokkiScreen.java
@@ -3,50 +3,30 @@ package com.OhtuProjekti.Screens;
 import com.OhtuProjekti.DBManager;
 import com.OhtuProjekti.Classes.Mokki;
 import com.OhtuProjekti.SceneManager;
-import com.OhtuProjekti.Utils;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+import javafx.scene.layout.Pane;
 
 import java.util.List;
 
-import static com.OhtuProjekti.Utils.*;
-
-
-/**
- * Class for handling mokkis
- */
 public class MokkiScreen extends SuperScreen {
 
-    /** Constructor, sets MokkiScreen.sceneManager
-     * @param sceneManager The common project SceneManager
-     */
     public MokkiScreen(SceneManager sceneManager) {
         this.sceneManager = sceneManager;
     }
 
-
-    /** Returns the Pane where the game is drawn
-     * @return returns GameScreen.screen
-     */
     @Override
-    public Pane getScreen(){
+    public Pane getScreen() {
         return screen;
     }
 
-
-    /**
-     * Creates the StackPane for drawing the screen
-     */
-    public void createScreen(){
+    public void createScreen() {
         super.createScreenSuper("Mökit");
-
 
         VBox contentBox = new VBox(10);
         contentBox.setPadding(new Insets(20));
@@ -54,32 +34,25 @@ public class MokkiScreen extends SuperScreen {
         Text title = new Text("Valitse mökki nähdäksesi tiedot:");
         contentBox.getChildren().add(title);
 
-        // Container for buttons
         VBox buttonBox = new VBox(5);
         buttonBox.setFillWidth(true);
-
 
         Text detailsText = new Text();
         detailsText.setWrappingWidth(400);
 
         List<Mokki> mokit = DBManager.getAllMokit();
-
         for (Mokki mokki : mokit) {
-            final Mokki currentMokki = mokki;  // Fix lambda capture
-
-            System.out.println("Mökki: " + currentMokki.nimi);
-            Button button = new Button(currentMokki.nimi);
+            Button button = new Button(mokki.nimi);
             button.setMaxWidth(Double.MAX_VALUE);
             button.setPrefWidth(200);
 
             button.setOnAction(e -> {
-                String details = "ID: " + currentMokki.mokkiID + "\n" +
-                        "Nimi: " + currentMokki.nimi + "\n" +
-                        "Osoite: " + currentMokki.osoite + "\n" +
-                        "Varustelu: " + currentMokki.varustelu + "\n" +
-                        "Hinta per yö: " + currentMokki.hintaPerYo + " €\n" +
-                        "Kapasiteetti: " + currentMokki.kapasiteetti + " hlö";
-
+                String details = "ID: " + mokki.mokkiID + "\n" +
+                        "Nimi: " + mokki.nimi + "\n" +
+                        "Osoite: " + mokki.osoite + "\n" +
+                        "Varustelu: " + mokki.varustelu + "\n" +
+                        "Hinta per yö: " + mokki.hintaPerYo + " €\n" +
+                        "Kapasiteetti: " + mokki.kapasiteetti + " hlö";
                 detailsText.setText(details);
             });
 
@@ -87,16 +60,47 @@ public class MokkiScreen extends SuperScreen {
         }
 
         contentBox.getChildren().addAll(buttonBox, detailsText);
+
+        GridPane grid = new GridPane();
+        TextField idField = new TextField();
+        TextField nameField = new TextField();
+        TextField addressField = new TextField();
+        TextField equipmentField = new TextField();
+        TextField priceField = new TextField();
+        TextField capacityField = new TextField();
+        Button insertButton = new Button("Insert Mökki");
+
+        grid.add(new Label("Mökki ID:"), 0, 0);
+        grid.add(idField, 1, 0);
+        grid.add(new Label("Nimi:"), 0, 1);
+        grid.add(nameField, 1, 1);
+        grid.add(new Label("Osoite:"), 0, 2);
+        grid.add(addressField, 1, 2);
+        grid.add(new Label("Varustelu:"), 0, 3);
+        grid.add(equipmentField, 1, 3);
+        grid.add(new Label("Hinta per yö:"), 0, 4);
+        grid.add(priceField, 1, 4);
+        grid.add(new Label("Kapasiteetti:"), 0, 5);
+        grid.add(capacityField, 1, 5);
+        grid.add(insertButton, 1, 6);
+
+        insertButton.setOnAction(e -> {
+            try {
+                int id = Integer.parseInt(idField.getText());
+                String nimi = nameField.getText();
+                String osoite = addressField.getText();
+                String varustelu = equipmentField.getText();
+                double hinta = Double.parseDouble(priceField.getText());
+                int kapasiteetti = Integer.parseInt(capacityField.getText());
+
+                Mokki mokki = new Mokki(id, nimi, osoite, varustelu, hinta, kapasiteetti);
+                DBManager.insertMokki(mokki);
+            } catch (Exception _) {
+            }
+        });
+
+        contentBox.getChildren().add(grid);
+
         screen.setCenter(contentBox);
-
-
-
-
-
     }
-
-
-
-
-
 }
