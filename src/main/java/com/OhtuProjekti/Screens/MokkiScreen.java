60f3ad4f6cac98f81b06e7d67ef4aa8c0d196465
Justus Talvitie <justus.talvitie@protonmail.com>
2025-05-12 15:08:39 +0300

    Tein paskaa koodia pliis muuttakaa sitä

--- src/main/java/com/OhtuProjekti/Screens/MokkiScreen.java
+++ src/main/java/com/OhtuProjekti/Screens/MokkiScreen.java
@@ -4,13 +4,17 @@ import com.OhtuProjekti.DBManager;
 import com.OhtuProjekti.Classes.Mokki;
 import com.OhtuProjekti.SceneManager;
 import com.OhtuProjekti.Utils;
+import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.GridPane;
 import javafx.scene.layout.Pane;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.scene.text.Text;
+
+import java.util.List;
 
 import static com.OhtuProjekti.Utils.*;
 
@@ -20,7 +24,9 @@ import static com.OhtuProjekti.Utils.*;
  */
 public class MokkiScreen extends SuperScreen {
 
-    /** Constructor, sets MokkiScreen.sceneManager
+    /**
+     * Constructor, sets MokkiScreen.sceneManager
+     *
      * @param sceneManager The common project SceneManager
      */
     public MokkiScreen(SceneManager sceneManager) {
@@ -28,11 +34,13 @@ public class MokkiScreen extends SuperScreen {
     }
 
 
-    /** Returns the Pane where the game is drawn
+    /**
+     * Returns the Pane where the game is drawn
+     *
      * @return returns GameScreen.screen
      */
     @Override
-    public Pane getScreen(){
+    public Pane getScreen() {
         return screen;
     }
 
@@ -40,17 +48,49 @@ public class MokkiScreen extends SuperScreen {
     /**
      * Creates the StackPane for drawing the screen
      */
-    public void createScreen(){
+
+    public void createScreen() {
         super.createScreenSuper("Mökit");
 
+        VBox contentBox = new VBox(10);
+        contentBox.setPadding(new Insets(20));
+
+        Text title = new Text("Valitse mökki nähdäksesi tiedot:");
+        contentBox.getChildren().add(title);
+
+        // Container for buttons
+        VBox buttonBox = new VBox(5);
+        buttonBox.setFillWidth(true);
 
 
+        Text detailsText = new Text();
+        detailsText.setWrappingWidth(400);
 
+        List<Mokki> mokit = DBManager.getAllMokit();
 
+        for (Mokki mokki : mokit) {
+            final Mokki currentMokki = mokki;  // Fix lambda capture
+
+            System.out.println("Mökki: " + currentMokki.nimi);
+            Button button = new Button(currentMokki.nimi);
+            button.setMaxWidth(Double.MAX_VALUE);
+            button.setPrefWidth(200);
+
+            button.setOnAction(e -> {
+                String details = "ID: " + currentMokki.mokkiID + "\n" +
+                        "Nimi: " + currentMokki.nimi + "\n" +
+                        "Osoite: " + currentMokki.osoite + "\n" +
+                        "Varustelu: " + currentMokki.varustelu + "\n" +
+                        "Hinta per yö: " + currentMokki.hintaPerYo + " €\n" +
+                        "Kapasiteetti: " + currentMokki.kapasiteetti + " hlö";
+
+                detailsText.setText(details);
+            });
+
+            buttonBox.getChildren().add(button);
+        }
+
+        contentBox.getChildren().addAll(buttonBox, detailsText);
+        screen.getChildren().add(contentBox);
     }
-
-
-
-
-
 }
